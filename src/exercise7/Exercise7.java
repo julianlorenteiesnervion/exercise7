c0afe50e1ff44aa668d1ec9462e08e6f005349f1
julian.lorente <julian.lorente@106-16.info2>
2024-09-20 14:19:37 +0200

    Ejercicio 7 terminado

---
+++ src/exercise7/Exercise7.java
@@ -0,0 +1,15 @@
+package exercise7;
+
+public class Exercise7 {
+
+	public static void main(String[] args) {
+		System.out.println("***********   *         *       *");
+		System.out.println("     *        *         **     **");
+		System.out.println("     *        *         * *   * *");
+		System.out.println("     *        *         *  * *  *");
+		System.out.println("     *        *         *   *   *");
+		System.out.println("******        *******   *       *");
+
+	}
+
+}
